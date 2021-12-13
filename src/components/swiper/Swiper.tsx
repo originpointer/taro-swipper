fe40201ff87685902dc15104b324e76819a61716
sodatodo <54822179+sodatodo@users.noreply.github.com>
2021-12-13 18:00:34 +0800

    Update Swiper.tsx

--- src/components/swiper/Swiper.tsx
+++ src/components/swiper/Swiper.tsx
@@ -1,12 +1,15 @@
 import { ViewProps } from '@tarojs/components/types/View'
 import { FC } from '@tarojs/taro'
 import { ITouchEvent, View } from '@tarojs/components'
-import { CSSProperties, useCallback, useMemo, useState } from 'react'
+import { CSSProperties, useCallback, useMemo, useRef, useState } from 'react'
+import _clamp from 'lodash/clamp'
 import { useUpdate } from '~/hooks'
 
 import { useTouch } from './utils/touch'
 import { SwiperDirection } from './swiper.shared'
 import { addUnitPx, unitToPx } from './utils/format/unit'
+import { useRenderedRef } from './utils/state'
+import { getRect } from './utils/dom/rect'
 
 export interface SwiperProps extends ViewProps {
   touchable?: boolean
@@ -18,13 +21,61 @@ const Swiper: FC<SwiperProps> = (props) => {
 
   const vertical = direction === 'vertical'
 
+  const rootRef = useRef()
+
+  // 用于描述矩形范围
+  const rectRef = useRef()
+
   const touch = useTouch()
 
   const update = useUpdate()
 
   const [offset, setOffset] = useState(0)
 
+  const activeIndexRef = useRef<number>(0)
+
   const [swiperTag, setSwiperTag] = useState(0)
+
+  const getTargetActive = useCallback((pace: number) => {
+    if (pace) {
+      return
+    }
+    return activeIndexRef.current
+  }, [])
+
+  const minOffsetRef = useRenderedRef(() => {
+    if (rectRef.current) {
+    }
+    return 0
+  })
+  // const maxCountRef = useRenderedRef(() => {
+  //   Math.ceil(Math.abs())
+  // })
+
+  const getTrackRect = useCallback(
+    () => {
+      // Promise.all([])
+      getRect(rootRef).then(rect => {
+        console.log(`sodalog rect`, rect)
+      })
+    },
+    []
+  )
+  const initialize = useCallback(
+    async () => {
+      if (!rootRef.current) return
+
+      await getTrackRect()
+    },
+    [getTrackRect]
+  )
+
+  useMounted(initialize)
+
+  const moveTo = useCallback(({ pace = 0, offset = 0, emitChange = false }) => {
+    const targetActive = getTargetActive(pace)
+    // _clamp(activeIndexRef.current + pace, 0, maxCountRef.current)
+  }, [])
 
   const onTouchStart = useCallback(
     (event: ITouchEvent) => {
@@ -55,7 +106,7 @@ const Swiper: FC<SwiperProps> = (props) => {
   console.log(`trackStyle`, trackStyle)
 
   return (
-    <View {...restProps}>
+    <View ref={rootRef} {...restProps}>
       <View
         catchMove
         onTouchStart={onTouchStart}
@@ -70,4 +121,8 @@ const Swiper: FC<SwiperProps> = (props) => {
 }
 
 export default Swiper
+
+function useMounted(initialize: () => Promise<void>) {
+  throw new Error('Function not implemented.')
+}
 
